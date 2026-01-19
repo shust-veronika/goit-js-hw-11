bbb1854aa3cd068ae124666101685641d102ab69
veronika <sustv153@gmail.com>
2026-01-19 19:56:46 +0200

    f

---
+++ src/main.js
@@ -0,0 +1,6 @@
+// import  form './js/pixabay-api';
+// import  form './js/render-functions';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
