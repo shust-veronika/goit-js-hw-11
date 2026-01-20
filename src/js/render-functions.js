967999c0ef17625114e6a1c3bb8abc2aeec6fc57
veronika <sustv153@gmail.com>
2026-01-20 09:38:03 +0200

    is-hidden

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -13,13 +13,13 @@ export function createGallery(images) {
     .map(
         ({ webformatURL, largeImageURL, tags, likes, views, comments, downloads}) =>
             `<li class="gallery-item">
-                <a class="fallery-link" href="${largeImageURL}">
+                <a class="gallery-link" href="${largeImageURL}">
                     <img class="gallery-image" src="${webformatURL}" alt="${tags}"/>
                     <div class="info">
                       <p><b>Likes:</b> ${likes}</p>
-                      <p><b>Views</b>${views}</p>
-                      <p><b>Comments</b>${comments}</p>
-                      <p><b>Downloads</b>${downloads}</p>
+                      <p><b>Views:</b>${views}</p>
+                      <p><b>Comments:</b>${comments}</p>
+                      <p><b>Downloads:</b>${downloads}</p>
                     </div>
                 </a>
             </li>`
